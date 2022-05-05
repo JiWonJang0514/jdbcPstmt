a8e8d1a9851ef93f7414cd4ae016453fd261b8ac
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2022-05-05 10:29:21 +0900

    commit

---
+++ Project0504/WebContent/qauery.sql
@@ -0,0 +1,12 @@
+drop table test;
+
+select * from test;
+
+create table test(
+	id varchar2(5),
+	pwd varchar(5)
+);
+
+insert into test values ('aa', '11');
+insert into test values ('bb', '22');
+insert into test values ('bb', '33');
